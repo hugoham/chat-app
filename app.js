ba8623154cf3a7ac14c2fe1ff4520d1c7a57956a
Hugo Hamelryck <hugo.hamelryck@gmail.com>
2015-03-18 11:32:37 +0100

    set start script

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 // respond with "Hello World!" on the homepage
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send('Hello World!');
-})
+});
 
 // accept POST request on the homepage
-app.post('/', function (req, res) {
+app.post('/', function(req, res) {
   res.send('Got a POST request');
-})
+});
 
 // accept PUT request at /user
-app.put('/user', function (req, res) {
+app.put('/user', function(req, res) {
   res.send('Got a PUT request at /user');
-})
+});
 
 // accept DELETE request at /user
-app.delete('/user', function (req, res) {
+app.delete('/user', function(req, res) {
   res.send('Got a DELETE request at /user');
-})
+});
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port);
 
-})
+});
